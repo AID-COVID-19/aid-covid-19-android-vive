b4945e7a5f3a8b7aaeecb8b03fd254ab34127c5d
Carlos Alberto Murillo <carlosalbertomurillo@gmail.com>
2020-04-04 22:17:53 -0400

    checking if user is loggin or not in start activity, to redirect

--- app/src/main/java/com/ai/covid19/tracking/android/AuthActivity.kt
+++ app/src/main/java/com/ai/covid19/tracking/android/AuthActivity.kt
@@ -1,28 +1,41 @@
 package com.ai.covid19.tracking.android
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.amazonaws.mobile.client.AWSMobileClient
 import com.amazonaws.mobile.client.Callback
+import com.amazonaws.mobile.client.UserState
 import com.amazonaws.mobile.client.UserStateDetails
 
 class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
-        AWSMobileClient.getInstance()
-            .initialize(applicationContext, object : Callback<UserStateDetails?> {
-                override fun onError(e: Exception?) {
-                    Log.e("INIT", "Initialization error.", e)
+
+        AWSMobileClient.getInstance().initialize(
+            applicationContext,
+            object : Callback<UserStateDetails> {
+                override fun onResult(userStateDetails: UserStateDetails) {
+                    when (userStateDetails.userState) {
+                        UserState.SIGNED_IN -> runOnUiThread {
+                            val intent = Intent(baseContext, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        UserState.SIGNED_OUT -> runOnUiThread {
+                            setContentView(R.layout.activity_auth)
+                            Log.d(this.javaClass.canonicalName, "User not is not logged in.")
+                        }
+                        else -> AWSMobileClient.getInstance().signOut()
+                    }
                 }
 
-                override fun onResult(result: UserStateDetails?) {
-                    Log.i("INIT", "onResult: " + result!!.userState)
+                override fun onError(e: java.lang.Exception) {
+                    Log.e("INIT", e.toString())
                 }
-            }
-            )
+            })
 
     }
 }
