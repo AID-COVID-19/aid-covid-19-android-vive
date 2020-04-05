eb000e01e4a6afd15effef08bb23dc0220cf1f71
Carlos Alberto Murillo <carlosalbertomurillo@gmail.com>
2020-04-05 03:32:14 -0400

    Refactor view fragment Check 1, view model

--- app/src/main/java/com/ai/covid19/tracking/android/ui/check/Check1Fragment.kt
+++ app/src/main/java/com/ai/covid19/tracking/android/ui/check/Check1Fragment.kt
@@ -5,24 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.ai.covid19.tracking.android.R
+import com.ai.covid19.tracking.android.databinding.FragmentCheck1Binding
 import kotlinx.android.synthetic.main.fragment_check_1.button_next_check_1
 
-class Check1Fragment : Fragment() {
+class Check1Fragment : Fragment(), View.OnClickListener {
 
-    private lateinit var viewModel: Check1ViewModel
+    private val viewModelCheck1: CheckViewModel by viewModels()
+    private lateinit var binding: FragmentCheck1Binding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        viewModel =
-                ViewModelProviders.of(this).get(Check1ViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_check_1, container, false)
-        return root
+        binding = FragmentCheck1Binding.inflate(inflater,container, false)
+        return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,5 +31,9 @@ class Check1Fragment : Fragment() {
             nav.navigate(R.id.action_check1Fragment_to_check2Fragment)
         }
         button_next_check_1.setOnClickListener(listener)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
