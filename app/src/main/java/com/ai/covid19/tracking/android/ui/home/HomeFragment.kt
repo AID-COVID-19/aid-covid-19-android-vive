6897ba851f6d80cc7cb4f527f8896b2edefb500a
Edwin González <edwinderepuesto@gmail.com>
2020-04-13 13:01:46 -0400

    Using the new ViewModelProvider builder, little cleanup

--- app/src/main/java/com/ai/covid19/tracking/android/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ai/covid19/tracking/android/ui/home/HomeFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.ai.covid19.tracking.android.R
 
 class HomeFragment : Fragment() {
@@ -15,12 +15,11 @@ class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
