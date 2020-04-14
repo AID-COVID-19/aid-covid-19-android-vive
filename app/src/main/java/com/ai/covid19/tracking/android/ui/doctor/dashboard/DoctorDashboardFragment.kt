554fea62801a916da33b0ad920e8b49e689b8fc0
Carlos Alberto Murillo <carlosalbertomurillo@gmail.com>
2020-04-14 01:37:15 -0400

    recycler view library for android x added, fixed the name of the fragment in dashboard fragment

--- app/src/main/java/com/ai/covid19/tracking/android/ui/doctor/dashboard/DoctorDashboardFragment.kt
+++ app/src/main/java/com/ai/covid19/tracking/android/ui/doctor/dashboard/DoctorDashboardFragment.kt
@@ -6,12 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.ai.covid19.tracking.android.databinding.FragmentCheck1Binding
+import com.ai.covid19.tracking.android.databinding.FragmentDoctorDashboardBinding
 
 class DoctorDashboardFragment : Fragment() {
 
     private val viewModelDoctorDashboard: DoctorDashboardViewModel by activityViewModels()
-    private lateinit var binding: FragmentCheck1Binding
+    private lateinit var binding: FragmentDoctorDashboardBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
